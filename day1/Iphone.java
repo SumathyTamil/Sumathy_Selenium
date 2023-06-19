180f0b050c22d08a127b14906ae406eb8dfe60a7
SumathyTamil <137081505+SumathyTamil@users.noreply.github.com>
2023-06-19 19:49:19 +0530

    Add files via upload

---
+++ day1/Iphone.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Iphone {
+	
+	public void makeCall() {
+		System.out.println("This make is available to make a phone call");
+	}
+	public void model() {
+		System.out.println("Apple");
+	}
+
+
+}
