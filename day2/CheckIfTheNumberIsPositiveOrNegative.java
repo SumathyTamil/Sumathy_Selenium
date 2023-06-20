cd9a710f8ab0e91d4a9406cd84e613ff204ceaa6
SumathyTamil <137081505+SumathyTamil@users.noreply.github.com>
2023-06-20 18:24:26 +0530

    Add files via upload

---
+++ day2/CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int num = -40;
+       if(num>0)
+    	   System.out.println("The number is positive"+num);
+       else
+    	   System.out.println("The number is negative"+num);
+	}
+
+}
---
+++ day2/ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class ConvertANegativeNumberToPositiveNumber {
+	public static void main(String[] args) {
+		int number = -40;
+		if(number<0) {
+			int result = number * -1;
+			System.out.println("The converted number is "+result);
+		}
+	}
+
+}
